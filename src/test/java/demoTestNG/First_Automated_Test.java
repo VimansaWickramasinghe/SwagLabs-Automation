6a0717408cb52671e1f9e90815a0da3d1f275201
VimansaWickramasinghe <vwickramasinghe1998@gmail.com>
2023-12-30 09:59:38 +0530

    Login tests

--- src/test/java/demoTestNG/First_Automated_Test.java
+++ src/test/java/demoTestNG/First_Automated_Test.java
@@ -4,15 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class First_Automated_Test {
     WebDriver driver;
 
     //use before class bcz this should execute before first test method
-    @BeforeClass
+    @BeforeMethod
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -20,22 +19,66 @@ public class First_Automated_Test {
         driver.get("https://www.saucedemo.com/v1/");
     }
 
-    @AfterClass
+    @AfterMethod
     public void closeBrowser(){
         driver.quit();
     }
 
     @Test
-    public void Login(){
-
+    public void Login_both_correct(){
+    //Valid username and password
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-        driver.findElement(By.cssSelector("#inventory_filter_container > div"));
-        String name = driver.getTitle();
-        System.out.println(name);
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://www.saucedemo.com/v1/inventory.html");
+    }
+    @Test
+    public void Login_invalid_username(){
+        //Invalid username and password
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).sendKeys("Vima_user");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://www.saucedemo.com/v1/");
+    }
 
+    @Test
+    public void Login_invalid_password(){
+        //Invalid username and password
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).sendKeys("secret_sauce123");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://www.saucedemo.com/v1/");
+    }
 
+    @Test
+    public void Login_both_invalid(){
+        //Invalid username and password
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).sendKeys("standard_uservima");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).sendKeys("secret_sauce123");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://www.saucedemo.com/v1/");
+    }
+
+    @Test
+    public void Login_both_empty(){
+        //Invalid username and password
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).sendKeys("");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).sendKeys("");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://www.saucedemo.com/v1/");
     }
 
 
